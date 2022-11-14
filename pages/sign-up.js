8db3c33b6e08f92c99d9a6758752cc2751397dab
josh-miller <joshuae.miller100@gmail.com>
2022-11-14 14:51:08 +0100

    Added signup hero section

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,22 +1,38 @@
+import React, { useState } from "react";
 import { Input, Button } from "@components/index";
 
 export default function SignUp() {
+  const [email, setEmail] = useState(null);
+
+  const getEmailValue = (value) => setEmail(value);
+
   return (
-    <div></div>
+    <div className="w-full px-6 pt-16">
+      {/* Form Section */}
+      <div className="grid w-full grid-cols-1 grid-rows-1 place-items-center gap-y-9 tablet:gap-y-16 desktop:gap-y-20">
+        <div className="grid max-w-sm grid-cols-1 grid-rows-1 gap-y-1">
+          <h1 className="text-center font-heading text-h4-mobile font-bold text-neutral-900 tablet:text-h5-desktop">
+            Create an Account on Hamzry
+          </h1>
+          <p className="text-center font-body text-label-4 font-medium text-neutral-500">
+            Join 50k people making money and completing GIGs on Hamzry for free.
+            Get Started Now
+          </p>
+        </div>
+        {/* Email Form Below */}
+      </div>
+    </div>
   );
 }
 
-{/* <div className="mx-auto grid h-[100vh] w-full grid-cols-1 grid-rows-1">
-{/* Form Section */}
-{/* <div className="grid grid-cols-1 grid-rows-1 place-items-center gap-y-10 max-w-[352px] ">
+{
+  /* <div className="mx-auto grid h-[100vh] w-full grid-cols-1 grid-rows-1">
+{/* Form Section */
+}
+{
+  /* <div className="grid grid-cols-1 grid-rows-1 place-items-center gap-y-10 max-w-[352px] ">
   <div className="pt-14">
-    <h1 className="text-center text-h4-mobile font-bold text-neutral-900 tablet:text-h5-desktop">
-      Create an Account on Hamzry
-    </h1>
-    <p className="text-center text-label-4 font-medium text-neutral-500">
-      Join 50k people making money and completing GIGs on Hamzry for free.
-      Get Started Now
-    </p>
+
   </div>
   <form
     className="grid w-full grid-cols-1 grid-rows-1 gap-y-2"
@@ -44,4 +60,5 @@ export default function SignUp() {
     </div>
   </form>
 </div>
-</div> */}
+</div> */
+}
